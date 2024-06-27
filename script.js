cea6323d4481b7836dd29bb5284d191446ab4080
Gesine Fengler <110592817+Gesine93@users.noreply.github.com>
2024-06-27 18:54:13 +0200

    created the index.html and script.js created a js function that creates a grid

---
+++ script.js
@@ -0,0 +1,18 @@
+function createGrid(size) {
+    for (let i=1; i<=size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j=1; j<=size; j++) {
+            let column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        };
+        grid.appendChild(row);
+    }
+}
+
+addEventListener("DOMContentLoaded", (event) => {
+    let sizeGrid = 16;
+    let grid = document.querySelector("#grid");
+    createGrid(sizeGrid);
+});
